12e4c627a4dac218f717318d79133843a3b935d6
gustavostrunkis <gustavoandradestrunkis@gmail.com>
2023-12-26 20:15:23 -0300

    Refatoração (index.html, e a1.js) - 1

--- scripts/a1.js
+++ scripts/a1.js
@@ -1,11 +1,5 @@
 
 
-//window.addEventListener("resize", window_rezise_1);
-//
-//function window_rezise_1(){
-//    location.reload();
-//}
-
 if(sessionStorage.getItem("button_id_1") == null || sessionStorage.getItem("button_id_1") == ""){
     ty_item_2_onClick("item_1");
 }
@@ -15,31 +9,27 @@ else{
 
 function ty_item_2_onClick(button_id)
 {
-    //deleteChilds_2();
-
-    //console.log(button_id);
-    var test;
     sessionStorage.setItem("button_id_1", button_id);
 
     var e = document.querySelector(".iframe_1");
 
-    let numberOfButtons_1 = 6;
+    let numberOfButtons = 6;
 
-    var e_button_1 = ["", "", "", "", "",""];
+    var eButton = ["", "", "", "", "",""];
 
-    for(let i1 = 1; i1 <= numberOfButtons_1; i1++)
+    for(let i1 = 1; i1 <= numberOfButtons; i1++)
     {
-        e_button_1[i1 - 1] = document.getElementById("item_" + i1.toString());
+        eButton[i1 - 1] = document.getElementById("item_" + i1.toString());
         var icon = document.getElementById("icon_" + i1.toString());
 
         if("item_" + i1.toString() == button_id)
         {
-            e_button_1[i1 - 1].style.setProperty("--s", 1);
+            eButton[i1 - 1].style.setProperty("--s", 1);
             icon.style.color = "#1e77fc";
         }
         else
         {
-            e_button_1[i1 - 1].style.setProperty("--s", 0);
+            eButton[i1 - 1].style.setProperty("--s", 0);
             icon.style.color = "white";
         }
     }
